2f87c1ac230eeccc6c7ef8cd3d9383a189a73b97
Dhivya G <zcsu037@zcs-ad.zybisys.com>
2023-07-19 13:27:25 +0530

    output

---
+++ index.js
@@ -0,0 +1,60 @@
+// Get elements
+const noteTitleInput = document.getElementById('noteTitle');
+const noteContentInput = document.getElementById('noteContent');
+console.log(noteContentInput,"this is")
+const saveNoteButton = document.getElementById('saveNote');
+const noteList = document.getElementById('noteList');
+const newbtn2=document.createElement('button');
+let note=[]
+let notearr={
+    inputBoxValues:[]
+}
+saveNoteButton.addEventListener("click", (e) => {
+e.preventDefault();
+  const noteText = noteTitleInput.value;
+  const notecont=noteContentInput.value;
+//   const newDiv=document.createElement('div')
+  const newNote=document.createElement('li');
+  console.log(newNote)
+  newNote.innerText=noteText;
+ 
+noteList.appendChild(newNote)
+
+const newbtn1=document.createElement('button');
+newbtn1.setAttribute("id","editbtn")
+// console.log(newbtn1.id,"this is priya")
+newbtn1.innerText="edit";
+noteList.appendChild(newbtn1)
+
+const newbtn2=document.createElement('button');
+newbtn2.setAttribute("id","deletebtn")
+newbtn2.innerText="Delete";
+newbtn2.classList.add("delete");
+noteList.appendChild(newbtn2)
+
+for(let i=0;i<noteText.length ;i++){
+    // notearr.inputBoxValues.title.push(noteText[i])
+    notearr.inputBoxValues.title=noteText;
+    notearr.inputBoxValues.content=notecont;
+}
+for(let j=0;j<notecont.length ;j++){
+    // notearr.inputBoxValues.title.push(noteText[i])
+    notearr.inputBoxValues.title=noteText;
+    notearr.inputBoxValues.content=notecont;
+}
+
+
+note.push(notearr);
+    console.log(note);
+    console.log(noteText.length);
+    resetForm()
+})
+function resetForm(){
+    document.getElementById("noteTitle").value = "";
+        document.getElementById("noteContent").value = "";
+}
+function removeItem(e) {
+	e.target.parentElement.remove();
+}
+
+newbtn2.addEventListener("click", removeItem);
